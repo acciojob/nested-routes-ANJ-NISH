5b9574df8db049b13f1c635f73dbf9ab5b0d8606
Anurag Joshi <anuragj990@gmail.com>
2024-12-31 14:29:45 +0530

    1st try

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
-
 import React from "react";
 import './../styles/App.css';
+import { Layout } from "./Layout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Women } from "./Women";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="women" element={<Women />}>
+              <Route path="grooming" element={<p>Grooming</p>} />
+              <Route path="shirt" element={<p>Shirts</p>} />
+              <Route path="trouser" element={<p>Trouser</p>} />
+              <Route path="jewellery" element={<p>Jewellery</p>} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Layout.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link, Outlet, useLocation } from "react-router-dom";
+
+export const Layout = () => {
+
+    const location=useLocation();
+
+  return (
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/women">Women</Link></li>
+      </ul>
+      {location.pathname==="/" && <p>Index</p>}
+      <Outlet /> 
+    </div>
+  );
+};
